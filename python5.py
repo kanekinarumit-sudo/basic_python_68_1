a1b9d7014b6f9ac6405a05290ee833b5f84c1266
Narumit <Narumit@gmail.com>
2026-01-31 14:37:48 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,27 @@
+"""
+#
+# Loop
+#while loop
+#
+"""
+
+i = 1 
+while i < 5:
+    print(i)
+    if i == 3 :
+        break
+    i += 1
+
+
+"""
+#
+# Loop
+# For loop
+#
+"""
+fuuits = ["apple", "banana", "cherry"]
+for fruit in fuuits:    
+    print(fruit)
+    if fruit == "banana":
+        break
+    
