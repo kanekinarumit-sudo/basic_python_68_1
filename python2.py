e2965979063a6f594b6e5752abeaa5dddd7fc0ad
Narumit <Narumit@gmail.com>
2026-01-31 13:56:41 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,56 @@
+"""
+#
+#part: Python Operators
+#Arithemetic Operators
+#
+"""
+x = 4 + 5  # Addition
+y = 10 - 2  # Subtraction   
+z = 3 * 7 # Multiplication
+a = 8 / 2  # Division
+b = 10 % 3  # Modulus
+c = 2 ** 3  # Exponentiation
+
+"""
+#
+# Part: Python Operators
+# Assignment Operators
+#
+"""
+x = 5  
+x = x + 3
+x += 3
+
+"""
+#
+# Part: Python Operators
+#  Comparison Operators
+#
+"""
+a = 5
+b = 10
+print(a == b)  # Equal
+print(a != b)  # Not Equal  
+print(a > b)   # Greater Than
+print(a < b)   # Less Than
+print(a >= b)  # Greater Than or Equal To
+print(a <= b)  # Less Than or Equal To
+
+
+"""
+#
+# Part: Python Operators
+# Logical Operators
+#
+"""
+x = True
+y = False   
+print(x and y)  # Logical AND
+print(x or y)   # Logical OR
+print(not x)    # Logical NOT
+print(x & y)    # Logical AND
+print(x | y)    # Logical OR
+print(~x)       # Logical NOT
+a = 5
+b = 10
+print((a < 10) and (b > 5))  
