d4b21518764db328fafa471175ddebba171f0b8c
Narumit <Narumit@gmail.com>
2026-01-31 13:18:09 +0700

    Python variables

---
+++ python1.py
@@ -0,0 +1,42 @@
+"""
+# Part: Python Comment
+#
+"""
+
+
+
+"""
+v = s / t
+ความเร็ว = ระยะทาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World!")
+
+"""
+#
+# Part: Pvthon variables
+"""
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+# 
+# Part: Varible Namibg
+#
+"""
+
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+
